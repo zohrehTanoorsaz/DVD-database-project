d716edf53b73a488bc611367c88c2adfe2244b68
zohrehTanoorsaz <zohrehtanoorsaz@gmail.com>
2023-10-17 21:53:11 -0400

    Length of rent for every Customer
    Length of rent for every Customer

---
+++ Query 6/Lenght of Rent per Customer.sql
@@ -0,0 +1,22 @@
+select 
+rental_id,
+Customer.Customer_id,
+concat(Customer.first_name,' _ ',customer.last_name) as Customerfullname,
+film.title as FilmTitle,
+rental_date,
+return_date,
+EXTRACT(DAY FROM MAX(return_date)-MIN(rental_date)) AS Length_of_Rent
+
+from rental join customer on customer.customer_id = rental.customer_id
+join inventory on inventory.inventory_id = rental.inventory_id
+join film on film.film_id = inventory.film_id
+
+where return_date is not null
+
+group by 
+rental_id,
+Customer.Customer_id,
+concat(Customer.first_name,' _ ',customer.last_name) ,
+film.title
+order by Length_of_Rent desc, Customerfullname desc, filmtitle asc
+
